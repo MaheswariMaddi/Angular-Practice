aaf62a2153f7fc026dae564e32927ec86216eecc
Maheswari Maddi <maddimaheswari111@gmail.com>
2024-04-09 19:44:28 +0530

    adding to table

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -16,20 +16,22 @@ export class GithubComponent {
   githubUserService: GithubUserDataService;
   name: string;
   userdetails:GithubUserProfile | null;
-
+  s:Array<GithubUserProfile>;
 
 
   constructor(githubUserServiceObj: GithubUserDataService) {
     this.githubUserService = githubUserServiceObj;
     this.name = '';
     this.userdetails=null;
+    this.s=new Array<GithubUserProfile>();
   }
 
   getUserData() {
     this.githubUserService.getUserProfileByUserName(this.name)
                           .subscribe(data => {
                              console.log(data);
-                             this.userdetails=data                          
+                             this.userdetails=data;     
+                             this.s.push(this.userdetails);                  
 
                           });
 
